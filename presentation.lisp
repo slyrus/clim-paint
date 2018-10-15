348d71e7ea4733739f13389f1c23e885e5a34e08
Cyrus Harmon <cyrus@bobobeach.com>
2018-10-14 20:57:01 -0700

    rework dragging-output* macro to use invoke-with-... style

--- presentation.lisp
+++ presentation.lisp
@@ -28,58 +28,69 @@ of pane."
                                          finish-on-release
                                          multiple-window)
                                &body body)
-    (declare (ignore repaint finish-on-release multiple-window feedback))
-    (let ((record (gensym "record"))
-          (feedback-fn (gensym "feedback-fn"))
-          (draw-feedback-fn (gensym "feedback-fn")))
-      `(let* ((,record (with-output-to-output-record (,stream)))
-              (,draw-feedback-fn
-               ,@body)
-              (,feedback-fn
-               (lambda
-                   (record stream initial-x initial-y x y event)
-                 (with-accessors ((shapes shapes))
-                     *application-frame*)
-                 (multiple-value-bind (record-x record-y)
-                     (output-record-position record)
-                   #+(or)
-                   (let ((erase-final t))
-                     (finish-on-release t)
-                     (flet ((simple-erase ()
-                              (when erase-final
-                                (when (output-record-parent record)
-                                  (delete-output-record record (output-record-parent record)))
-                                (climi::with-double-buffering
-                                    ((stream record) (buffer-rectangle))
-                                  (stream-replay stream buffer-rectangle)))))))
-                   (let ((dx (- record-x initial-x))
-                         (dy (- record-y initial-y)))
-                     (case event
-                       (:draw
-                        (with-bounding-rectangle* (old-x1 old-y1 old-x2 old-y2)
-                            record
-                          (when (output-record-parent record)
-                            (delete-output-record record (output-record-parent record)))
-                          (setf (output-record-position record)
-                                (values (+ dx x) (+  dy y)))
-                          (add-output-record (funcall ,draw-feedback-fn stream x y) record)
-                          (stream-add-output-record stream record)
-                          (with-bounding-rectangle* (new-x1 new-y1 new-x2 new-y2)
-                              record
-                            (multiple-value-bind (area-x1 area-y1 area-x2 area-y2)
-                                (climi::bound-rectangles old-x1 old-y1 old-x2 old-y2
-                                                         new-x1 new-y1 new-x2 new-y2)
-                              (climi::with-double-buffering
-                                  ((stream area-x1 area-y1 area-x2 area-y2)
-                                   (buffer-rectangle))
-                                (stream-replay stream buffer-rectangle))))))
-                       (:erase
-                        (with-bounding-rectangle* (x1 y1 x2 y2)
-                            record
-                          (clear-output-record record)
-                          (climi::with-double-buffering
-                              ((stream x1 y1 x2 y2)
-                               (buffer-rectangle))
-                            (stream-replay stream buffer-rectangle))))))))))
-        (drag-output-record ,stream ,record :erase-final t ,@args :feedback ,feedback-fn)))))
+    "An enhanced version of the clim:dragging-output macro. The
+original macro doesn't take a feedback function, but this one does and
+the feeedback function is then passed to drag-output-record. The
+feedback function is provided as the body in calls to this macro and
+must consist of a function that takes three arguments, the stream, and
+teh x and y position of the pointer, and returns an output record of
+the drawn feedback. This output record is inserted into the stream's
+current output-record upon drawing, and is subsequently removed upon
+erasing."
+    `(invoke-with-dragging-output* ,@body ,stream ,@args)))
 
+(locally
+    (declare #+sbcl (sb-ext:muffle-conditions style-warning))
+  (defun invoke-with-dragging-output* (continuation
+                                       &optional (stream *standard-output*)
+                                       &rest args
+                                       &key repaint
+                                            finish-on-release
+                                            multiple-window)
+    (declare (ignore repaint finish-on-release multiple-window))
+    (let ((record (with-output-to-output-record (stream))))
+      (flet ((feedback-fn (record stream initial-x initial-y x y event)
+               (with-accessors ((shapes shapes))
+                   *application-frame*)
+               (multiple-value-bind (record-x record-y)
+                   (output-record-position record)
+                 #+(or)
+                 (let ((erase-final t))
+                   (finish-on-release t)
+                   (flet ((simple-erase ()
+                            (when erase-final
+                              (when (output-record-parent record)
+                                (delete-output-record record (output-record-parent record)))
+                              (climi::with-double-buffering
+                                  ((stream record) (buffer-rectangle))
+                                (stream-replay stream buffer-rectangle)))))))
+                 (let ((dx (- record-x initial-x))
+                       (dy (- record-y initial-y)))
+                   (case event
+                     (:draw
+                      (with-bounding-rectangle* (old-x1 old-y1 old-x2 old-y2)
+                          record
+                        (when (output-record-parent record)
+                          (delete-output-record record (output-record-parent record)))
+                        (setf (output-record-position record)
+                              (values (+ dx x) (+  dy y)))
+                        (add-output-record (funcall continuation stream x y) record)
+                        (stream-add-output-record stream record)
+                        (with-bounding-rectangle* (new-x1 new-y1 new-x2 new-y2)
+                            record
+                          (multiple-value-bind (area-x1 area-y1 area-x2 area-y2)
+                              (climi::bound-rectangles old-x1 old-y1 old-x2 old-y2
+                                                       new-x1 new-y1 new-x2 new-y2)
+                            (climi::with-double-buffering
+                                ((stream area-x1 area-y1 area-x2 area-y2)
+                                 (buffer-rectangle))
+                              (stream-replay stream buffer-rectangle))))))
+                     (:erase
+                      (with-bounding-rectangle* (x1 y1 x2 y2)
+                          record
+                        (clear-output-record record)
+                        (climi::with-double-buffering
+                            ((stream x1 y1 x2 y2)
+                             (buffer-rectangle))
+                          (stream-replay stream buffer-rectangle)))))))))
+        (apply #'drag-output-record stream record :erase-final t :feedback #'feedback-fn args)))))
