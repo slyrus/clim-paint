2cd94af0fcf01aff95a3e9729e24b3f3b8f2aa0a
Cyrus Harmon <cyrus@bobobeach.com>
2018-11-26 08:58:37 -0800

    WIP on rectangle resizing
     * still need to access the proper point via selection
    
     * now store rectangles as two points that define the rectangle,
       rather than as the rectangle itself

--- rectangle.lisp
+++ rectangle.lisp
@@ -4,10 +4,8 @@
 ;;;
 ;;; rectangle
 (defclass paint-rectangle (paint-object)
-  ((rectangle :type rectangle :initarg rectangle :accessor %rectangle)))
-
-(defmethod shared-initialize :after ((rectangle paint-rectangle) slot-names &key x1 y1 x2 y2)
-  (setf (%rectangle rectangle) (make-rectangle* x1 y1 x2 y2)))
+  ((point-1 :type point :initarg :point-1 :accessor %point-1)
+   (point-2 :type point :initarg :point-2 :accessor %point-2)))
 
 (defgeneric paint-rectangle-p (object)
   (:method ((object t)) nil)
@@ -17,10 +15,10 @@
 (defun make-paint-rectangle (x1 y1 x2 y2 &key (ink nil ink-supplied-p)
                                               (filled nil filled-supplied-p))
   (apply #'make-instance 'paint-rectangle
-         :x1 (coerce x1 'coordinate)
-         :y1 (coerce y1 'coordinate)
-         :x2 (coerce x2 'coordinate)
-         :y2 (coerce y2 'coordinate)
+         :point-1 (make-point (coerce x1 'coordinate)
+                              (coerce y1 'coordinate))
+         :point-2 (make-point (coerce x2 'coordinate)
+                              (coerce y2 'coordinate))
          (append
           (when ink-supplied-p
             `(:ink ,ink))
@@ -28,7 +26,11 @@
             `(:filled ,filled)))))
 
 (defmethod bounding-rectangle* ((object paint-rectangle))
-  (bounding-rectangle* (%rectangle object)))
+  (multiple-value-bind (x1 y1)
+      (point-position (%point-1 object))
+    (multiple-value-bind (x2 y2)
+        (point-position (%point-2 object))
+      (values x1 y1 x2 y2))))
 
 ;;;
 ;;; rectangle-presentation
@@ -41,39 +43,39 @@
 (defun draw-rectangle-selection (pane rectangle &key (ink +black+)
                                                      (radius 8)
                                                      (filled nil))
-  (multiple-value-bind (x1 y1 x2 y2)
-      (bounding-rectangle* rectangle)
-    (let ((sx1 (if (>= x2 x1)
-                   (- x1 *rectangle-selection-width*)
-                   (+ x1 *rectangle-selection-width* -1)))
-          (sy1 (if (>= y2 y1)
-                         (- y1 *rectangle-selection-width*)
-                         (+ y1 *rectangle-selection-width* -1)))
-          (sx2 (if (>= x2 x1)
-                         (+ x2 *rectangle-selection-width* -1)
-                         (- x2 *rectangle-selection-width*)))
-          (sy2 (if (>= y2 y1)
-                         (+ y2 *rectangle-selection-width* -1)
-                         (- y2 *rectangle-selection-width*))))
-      (draw-rectangle* pane sx1 sy1 sx2 sy2
-                       :ink *selection-color*
-                       :filled nil
-                       :line-dashes t)
-      (map nil
-           (lambda (x)
-             (present (make-instance 'selection-handle-point
-                                     :paint-object rectangle
-                                     :point (apply #'make-point x)
-                                     :ink ink
-                                     :radius radius
-                                     :filled filled)
-                      'selection-handle-point
-                      :record-type 'selection-handle-point-presentation
-                      :single-box t))
-           (list (list sx1 sy1)
-                 (list sx2 sy1)
-                 (list sx2 sy2)
-                 (list sx1 sy2))))))
+  (multiple-value-bind (x1 y1)
+      (point-position (%point-1 rectangle))
+    (multiple-value-bind (x2 y2)
+        (point-position (%point-2 rectangle))
+      (let ((sx1 (if (>= x2 x1)
+                     (- x1 *rectangle-selection-width*)
+                     (+ x1 *rectangle-selection-width* -1)))
+            (sy1 (if (>= y2 y1)
+                     (- y1 *rectangle-selection-width*)
+                     (+ y1 *rectangle-selection-width* -1)))
+            (sx2 (if (>= x2 x1)
+                     (+ x2 *rectangle-selection-width* -1)
+                     (- x2 *rectangle-selection-width*)))
+            (sy2 (if (>= y2 y1)
+                     (+ y2 *rectangle-selection-width* -1)
+                     (- y2 *rectangle-selection-width*))))
+        (draw-rectangle* pane sx1 sy1 sx2 sy2
+                         :ink *selection-color*
+                         :filled nil
+                         :line-dashes t)
+        (map nil
+             (lambda (x)
+               (present (make-instance 'selection-handle-point
+                                       :paint-object rectangle
+                                       :point (apply #'make-point x)
+                                       :ink ink
+                                       :radius radius
+                                       :filled filled)
+                        'selection-handle-point
+                        :record-type 'selection-handle-point-presentation
+                        :single-box t))
+             (list (list sx1 sy1)
+                   (list sx2 sy2)))))))
 
 (define-presentation-method present (rectangle (type paint-rectangle) pane
                                           (view clim-paint-view) &key)
@@ -93,21 +95,30 @@
 ;;; highlighting
 (defparameter *rectangle-highlight-margin* 2)
 
+
+#+nil
+(multiple-value-bind (x1 y1)
+    (point-position (%point-1 rectangle))
+  (multiple-value-bind (x2 y2)
+      (point-position (%point-2 rectangle))))
+
 (define-presentation-method highlight-presentation
     ((type paint-rectangle) (record rectangle-presentation) stream state)
-  (let ((paint-rectangle (presentation-object record)))
+  (let ((rectangle (presentation-object record)))
     (case state
       (:highlight
-       (multiple-value-bind (x1 y1 x2 y2)
-        (bounding-rectangle* paint-rectangle)
-         (draw-rectangle* stream
-                          (- x1 *rectangle-highlight-margin* 1)
-                          (- y1 *rectangle-highlight-margin* 1)
-                          (+ x2 *rectangle-highlight-margin*)
-                          (+ y2 *rectangle-highlight-margin*)
-                          :ink *highlight-color*
-                          :filled nil
-                          :line-thickness *rectangle-highlight-margin*)))
+       (multiple-value-bind (x1 y1)
+           (point-position (%point-1 rectangle))
+         (multiple-value-bind (x2 y2)
+             (point-position (%point-2 rectangle))
+           (draw-rectangle* stream
+                            (- x1 *rectangle-highlight-margin* 1)
+                            (- y1 *rectangle-highlight-margin* 1)
+                            (+ x2 *rectangle-highlight-margin*)
+                            (+ y2 *rectangle-highlight-margin*)
+                            :ink *highlight-color*
+                            :filled nil
+                            :line-thickness *rectangle-highlight-margin*))))
       (:unhighlight
        (queue-repaint
         stream
@@ -116,18 +127,20 @@
          :sheet stream
          :region (transform-region
                   (sheet-native-transformation stream)
-                  (multiple-value-bind (x1 y1 x2 y2)
-                      (bounding-rectangle* record)
-                    (make-rectangle* (- x1 *rectangle-highlight-margin* 2)
-                                     (- y1 *rectangle-highlight-margin* 2)
-                                     (+ x2 *rectangle-highlight-margin* 2)
-                                     (+ y2 *rectangle-highlight-margin* 2))))))))))
+                  (multiple-value-bind (x1 y1)
+                      (point-position (%point-1 rectangle))
+                    (multiple-value-bind (x2 y2)
+                        (point-position (%point-2 rectangle))
+                      (make-rectangle* (- x1 *rectangle-highlight-margin* 2)
+                                       (- y1 *rectangle-highlight-margin* 2)
+                                       (+ x2 *rectangle-highlight-margin* 2)
+                                       (+ y2 *rectangle-highlight-margin* 2)))))))))))
 ;;; 1. com-drag-move-rectangle
 (define-clim-paint-command (com-drag-move-rectangle)
-    ((paint-rectangle paint-rectangle))
+    ((rectangle paint-rectangle))
   (with-accessors ((ink ink)
                    (filled filledp))
-      paint-rectangle
+      rectangle
     (let ((pane (get-frame-pane *application-frame* 'app)))
       (multiple-value-bind (startx starty)
           (stream-pointer-position pane)
@@ -136,24 +149,28 @@
                 (pane :finish-on-release t)
               (lambda (stream x y)
                 (with-output-to-output-record (stream)
-                  (multiple-value-bind (x1 y1 x2 y2)
-                      (bounding-rectangle* paint-rectangle)
-                    (draw-rectangle* stream
-                                     (+ x1 (- x startx))
-                                     (+ y1 (- y starty))
-                                     (+ x2 (- x startx))
-                                     (+ y2 (- y starty))
-                                     :ink ink
-                                     :filled filled)))))
+                  (multiple-value-bind (x1 y1)
+                      (point-position (%point-1 rectangle))
+                    (multiple-value-bind (x2 y2)
+                        (point-position (%point-2 rectangle))
+                      (draw-rectangle* stream
+                                       (+ x1 (- x startx))
+                                       (+ y1 (- y starty))
+                                       (+ x2 (- x startx))
+                                       (+ y2 (- y starty))
+                                       :ink ink
+                                       :filled filled))))))
           ;; FIXME! probably want a better API here
-          (with-accessors ((rectangle %rectangle)) paint-rectangle
-            (multiple-value-bind (x1 y1 x2 y2)
-                (bounding-rectangle* paint-rectangle)
-              (setf rectangle (make-rectangle*
-                               (+ x1 (- x startx))
-                               (+ y1 (- y starty))
-                               (+ x2 (- x startx))
-                               (+ y2 (- y starty)))))))))))
+          (multiple-value-bind (x1 y1)
+              (point-position (%point-1 rectangle))
+            (multiple-value-bind (x2 y2)
+                (point-position (%point-2 rectangle))
+              (setf (%point-1 rectangle) (make-point
+                                          (+ x1 (- x startx))
+                                          (+ y1 (- y starty)))
+                    (%point-2 rectangle) (make-point
+                                          (+ x2 (- x startx))
+                                          (+ y2 (- y starty)))))))))))
 
 ;;; 2. com-move-rectangle
 (define-clim-paint-command (com-move-rectangle)
@@ -173,39 +190,57 @@
     (object presentation)
   (list presentation))
 
+;; FIXME!!!
+(defun rectangle-other-point (rectangle x y)
+  (multiple-value-bind (x1 y1)
+      (point-position (%point-1 rectangle))
+    (multiple-value-bind (x2 y2)
+        (point-position (%point-2 rectangle))
+      (if (and (= x x1) (= y y1))
+          (values x2 y2)
+          (values x1 y1)))))
 
 (define-clim-paint-command (com-drag-move-rectangle-selection-handle)
-    ((object selection-handle-object))
-  (let ((paint-rectangle (paint-object object)))
+    ((selection-handle-point selection-handle-point))
+  (let ((rectangle (paint-object selection-handle-point)))
     (with-accessors ((ink ink)
                      (filled filledp))
-        paint-rectangle
+        rectangle
       (let ((pane (get-frame-pane *application-frame* 'app)))
         (multiple-value-bind (startx starty)
             (stream-pointer-position pane)
-          (multiple-value-bind (x y)
-              (dragging-output*
-                  (pane :finish-on-release t)
-                (lambda (stream x y)
-                  (with-output-to-output-record (stream)
-                    (multiple-value-bind (x1 y1 x2 y2)
-                        (bounding-rectangle* paint-rectangle)
-                      (draw-rectangle* stream
-                                       (+ x1 (- x startx))
-                                       (+ y1 (- y starty))
-                                       (+ x2 (- x startx))
-                                       (+ y2 (- y starty))
-                                       :ink ink
-                                       :filled filled)))))
-            ;; FIXME! probably want a better API here
-            (with-accessors ((rectangle %rectangle)) paint-rectangle
-              (multiple-value-bind (x1 y1 x2 y2)
-                  (bounding-rectangle* paint-rectangle)
-                (setf rectangle (make-rectangle*
-                                 (+ x1 (- x startx))
-                                 (+ y1 (- y starty))
-                                 (+ x2 (- x startx))
-                                 (+ y2 (- y starty))))))))))))
+          (multiple-value-bind (near-x near-y)
+              (point-position (%point selection-handle-point))
+            (multiple-value-bind (other-x other-y)
+                (rectangle-other-point rectangle near-x near-y)
+              (multiple-value-bind (x y)
+                  (dragging-output*
+                      (pane :finish-on-release t)
+                    (lambda (stream x y)
+                      (with-output-to-output-record (stream)
+                        (draw-rectangle* stream
+                                         (+ near-x (- x startx))
+                                         (+ near-y (- y starty))
+                                         other-x
+                                         other-y
+                                         :ink ink
+                                         :filled filled)
+                        (multiple-value-bind (x1 y1)
+                            (point-position (%point selection-handle-point))
+                          (draw-circle* stream
+                                        (+ x1 (- x startx))
+                                        (+ y1 (- y starty))
+                                        (radius selection-handle-point)
+                                        :ink (ink selection-handle-point)
+                                        :filled (filledp selection-handle-point)
+                                        :line-thickness 2)))))
+                ;; FIXME! probably want a better API here
+                (setf (%point-1 rectangle) (make-point
+                                            (+ near-x (- x startx))
+                                            (+ near-y (- y starty)))
+                      (%point-2 rectangle) (make-point
+                                            other-x
+                                            other-y))))))))))
 
 ;;; 4. com-move-rectangle-selection-handle
 (define-clim-paint-command (com-move-rectangle-selection-handle)
