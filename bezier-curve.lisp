ead959dfe66934e8bd862f97ac04c98f8e82bc74
Cyrus Harmon <cyrus@bobobeach.com>
2018-11-29 17:31:53 -0800

    first pass at bezier curve editing

--- bezier-curve.lisp
+++ bezier-curve.lisp
@@ -3,6 +3,7 @@
 
 (defclass paint-bezier-curve-segment (paint-object)
   ((segment :initarg :segment :accessor segment)
+   (index :initarg :index :accessor segment-index)
    (line-thickness :initarg :line-thickness :accessor line-thickness :initform 1)))
 
 (defclass paint-bezier-curve (paint-object)
@@ -84,8 +85,7 @@
                                                           (view clim-paint-view) &key)
   (draw-paint-bezier-curve-segment pane bezier-curve-segment))
 
-(defun draw-paint-bezier-curve (pane bezier-curve &key ink line-thickness)
-  (declare (ignore pane))
+(defun make-bezier-curve-segments (bezier-curve)
   (destructuring-bind (leftover segment-seq)
       (reduce (lambda (acc point)
                 (destructuring-bind (build vec)
@@ -106,13 +106,19 @@
               :initial-value (list nil (make-array 4 :fill-pointer 0)))
     (unless (equal (length leftover) 1)
       (error "Invalid point-seq: ~S ~S" bezier-curve leftover))
-    (loop for seg across segment-seq
-       do (present (make-instance 'paint-bezier-curve-segment
-                                  :segment seg
-                                  :ink ink
-                                  :line-thickness line-thickness)
-                   'paint-bezier-curve-segment
-                   :record-type 'bezier-curve-segment-presentation))))
+    segment-seq))
+
+(defun draw-paint-bezier-curve (pane bezier-curve &key ink line-thickness)
+  (declare (ignore pane))
+  (loop for seg across (make-bezier-curve-segments bezier-curve)
+     for i from 0
+     do (present (make-instance 'paint-bezier-curve-segment
+                                :segment seg
+                                :index i
+                                :ink ink
+                                :line-thickness line-thickness)
+                 'paint-bezier-curve-segment
+                 :record-type 'bezier-curve-segment-presentation)))
 
 (define-presentation-method present (bezier-curve (type paint-bezier-curve) pane
                                              (view clim-paint-view) &key)
@@ -124,6 +130,21 @@
   (if (gethash bezier-curve *selected-object-hash*)
       (draw-bezier-curve-selection pane bezier-curve)))
 
+;;;
+;;; refined-position test
+(define-presentation-method presentation-refined-position-test
+    ((type paint-bezier-curve-segment) (record bezier-curve-segment-presentation) x y)
+  (let ((bezier-curve-segment (presentation-object record)))
+    (let ((squared-distance (cdr (apply #'rough-closest-point-on-bezier-curve
+                                        (make-point x y)
+                                        (segment bezier-curve-segment)))))
+      (< squared-distance 100))))
+
+(define-presentation-method presentation-refined-position-test
+    ((type paint-bezier-curve) (record bezier-curve-presentation) x y)
+  nil)
+
+;;;
 ;;; highlighting
 (define-presentation-method highlight-presentation
     ((type paint-bezier-curve-segment) (record bezier-curve-segment-presentation) stream state)
@@ -193,33 +214,56 @@
 
 ;;; split a bezier curve
 
-(define-clim-paint-command (com-drag-split-bezier-curve)
-    ((bezier-curve paint-bezier-curve) (presentation presentation) (frame frame))
-  (declare (ignore presentation))
-  (with-accessors ((ink ink)
-                   (shapes shapes))
+(define-clim-paint-command (com-drag-split-bezier-curve-segment)
+    ((bezier-curve-segment paint-bezier-curve-segment) (presentation presentation) (frame frame))
+  (with-accessors ((ink ink))
       frame
     (let ((pane (get-frame-pane frame 'app))
-          (new-ink (or (ink bezier-curve) ink)))
-      (multiple-value-bind (x y)
-          (dragging-output (pane :finish-on-release t)
-            (draw-circle pane (get-pointer-position pane) 6
-                         :ink new-ink :filled t))
-        ;; here's where we need to find out which segment in the
-        ;; bezier curve was actually clicked on...
-        ))))
+          (new-ink (or (ink bezier-curve-segment) ink)))
+      (multiple-value-bind (startx starty)
+          (stream-pointer-position pane)
+        (let ((start-point (make-point startx starty)))
+          (let ((nearest-point (apply #'find-closest-point-on-bezier-curve start-point
+                                      (segment bezier-curve-segment))))
+            (draw-circle pane nearest-point 6
+                         :ink +yellow+ :filled t)
+            (multiple-value-bind (x y)
+                (dragging-output*
+                    (pane :finish-on-release t)
+                  (lambda (stream x y)
+                    (with-output-to-output-record (stream)
+                      (draw-circle* stream x y 6
+                                    :ink new-ink :filled t))))
+              (let ((new-point (make-point x y)))
+                (let ((bezier-curve (presentation-object (output-record-parent presentation)))
+                      (index (segment-index bezier-curve-segment)))
+                  (when bezier-curve
+                    (with-accessors
+                          ((control-points %control-points))
+                        bezier-curve
+                      (let ((left-control (control-point bezier-curve (+ 1 (* index 3))))
+                            (right-control (control-point bezier-curve (+ 2 (* index 3)))))
+                        (flexichain:insert-vector* control-points
+                                                   (+ 2 (* index 3))
+                                                   (vector (midpoint
+                                                            left-control
+                                                            new-point)
+                                                           new-point
+                                                           (midpoint
+                                                            new-point
+                                                            right-control)))))))))))))))
 
 ;;; com-split-line
-(define-clim-paint-command (com-split-bezier-curve)
+(define-clim-paint-command (com-split-bezier-curve-segment)
     ((presentation t))
-  (let ((bezier-curve (presentation-object presentation)))
-    (com-drag-split-bezier-curve bezier-curve presentation *application-frame*)))
+  (let ((bezier-curve-segment (presentation-object presentation)))
+    (com-drag-split-bezier-curve-segment bezier-curve-segment presentation *application-frame*)))
 
-(define-gesture-name split-bezier-curve-gesture :pointer-button (:left :control))
+(define-gesture-name split-bezier-curve-segment-gesture :pointer-button (:middle :control))
 
-(define-presentation-to-command-translator split-bezier-curve-translator
-    (paint-bezier-curve com-split-bezier-curve clim-paint
-          :gesture split-bezier-curve-gesture
+(define-presentation-to-command-translator split-bezier-curve-segment-translator
+    (paint-bezier-curve-segment com-split-bezier-curve-segment clim-paint
+          :gesture split-bezier-curve-segment-gesture
           :menu nil
           :tester ((object)
                    t))
