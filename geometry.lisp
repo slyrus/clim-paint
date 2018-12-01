c8625568dd7cdb69088af8ed7860fde93d29a700
Cyrus Harmon <cyrus@bobobeach.com>
2018-12-01 14:21:04 -0800

    fix find-closest-point-on-bezier-curve
     * the bug was that I had max where I needed min. I was trying to make
       sure I didn't go over 1, and ended up choosing 1 everytime. The
       other changes were made in the course of tracking down that bug but
       are probably good things to do anyway.
    
     * change order of arguments
       squared-distance-to-parameter-bezier-curve so that it takes the
       point in question first, then tee and the bezier control points.
    
     * squared-distance-to-parametric-bezier-curve now returns three
       values, first the squared-distance, as before, but two new values,
       the x and y coordinates of the point on the curve defined by
       tee. This can save us having to recompute the point.
    
     * explicitly check the ends of the curve. Perhaps we don't need this?
    
     * rework how we split the cruve up in refine-guess

--- geometry.lisp
+++ geometry.lisp
@@ -49,39 +49,62 @@
           (values (1d-parametric-bezier-curve tee p0x p1x p2x p3x)
                   (1d-parametric-bezier-curve tee p0y p1y p2y p3y)))))))
 
-(defun squared-distance-to-parametric-bezier-curve (tee point p0 p1 p2 p3)
+(defun squared-distance-to-parametric-bezier-curve (point tee p0 p1 p2 p3)
+  "Returns the square of the distance of POINT to the point on the
+bezier curved described by control points P0, P1, P2, P3, and
+evaluated at tee where t is in [0,1]."
   (multiple-value-bind (x y)
       (point-position point)
     (multiple-value-bind (bx by)
         (2d-parametric-bezier-curve tee p0 p1 p2 p3)
-      (+ (square (- bx x))
-         (square (- by y))))))
+      (values (+ (square (- bx x))
+                 (square (- by y)))
+              bx by))))
 
 (defun rough-closest-point-on-bezier-curve (point p0 p1 p2 p3 &key (steps 100))
   (let ((candidates
          (mapcar (lambda (x)
-                   (cons x (squared-distance-to-parametric-bezier-curve x point p0 p1 p2 p3)))
+                   (cons x (squared-distance-to-parametric-bezier-curve point x p0 p1 p2 p3)))
                  (loop for i from 0 to steps collect (float (/ i steps))))))
     (car (sort candidates #'< :key #'cdr))))
 
 (defun find-closest-point-on-bezier-curve (point p0 p1 p2 p3 &key (steps 100))
-  (let ((guess (car (rough-closest-point-on-bezier-curve point p0 p1 p2 p3 :steps steps))))
-    (let ((min-guess (max (- guess (float (/ steps))) 0))
-          (max-guess (max (+ guess (float (/ steps))) 1)))
-      (let ((tee (refine-guess min-guess max-guess point p0 p1 p2 p3 :steps steps)))
-        (multiple-value-call #'make-point (2d-parametric-bezier-curve tee p0 p1 p2 p3))))))
+  (multiple-value-bind (val0 x0 y0)
+      (squared-distance-to-parametric-bezier-curve
+       point 0 p0 p1 p2 p3)
+    (multiple-value-bind (val1 x1 y1)
+      (squared-distance-to-parametric-bezier-curve
+       point 1 p0 p1 p2 p3)
+      (let ((guess (car (rough-closest-point-on-bezier-curve
+                         point p0 p1 p2 p3 :steps steps))))
+        (let ((min-guess (max (- guess (float (/ steps))) 0))
+              (max-guess (min (+ guess (float (/ steps))) 1)))
+          (multiple-value-bind (tee guess-val)
+              (refine-guess min-guess max-guess point p0 p1 p2 p3)
+            (cond
+              ((<= guess-val (min val0 val1))
+               (multiple-value-call #'make-point
+                 (2d-parametric-bezier-curve tee p0 p1 p2 p3)))
+              ((<= val0 val1)
+               (make-point x0 y0))
+              (t
+               (make-point x1 y1)))))))))
 
-(defun refine-guess (min-guess max-guess point p0 p1 p2 p3 &key (steps 100) (epsilon 0.001))
-  (if (< (- max-guess min-guess) epsilon)
-      (/ (+ max-guess min-guess) 2)
-      (let ((mid-guess (/ (+ min-guess max-guess) 2)))
-        (let ((left-value (squared-distance-to-parametric-bezier-curve min-guess point p0 p1 p2 p3))
-              (mid-value (squared-distance-to-parametric-bezier-curve mid-guess point p0 p1 p2 p3))
-              (right-value (squared-distance-to-parametric-bezier-curve max-guess point p0 p1 p2 p3)))
+(defun refine-guess (left-guess right-guess point p0 p1 p2 p3 &key (epsilon 0.001))
+    (let ((mid-guess (/ (+ left-guess right-guess) 2)))
+    (let ((left-value (squared-distance-to-parametric-bezier-curve point left-guess p0 p1 p2 p3))
+          (mid-value (squared-distance-to-parametric-bezier-curve point mid-guess p0 p1 p2 p3))
+          (right-value (squared-distance-to-parametric-bezier-curve point right-guess p0 p1 p2 p3)))
+      (if (< (- right-guess left-guess) epsilon)
+          (cond ((<= mid-value (min left-value right-value))
+                 (values mid-guess mid-value))
+                ((<= left-value right-value)
+                 (values left-guess left-value))
+                (t (values right-guess right-value)))
           (if (< (abs (- left-value mid-value))
                  (abs (- right-value mid-value)))
-              (refine-guess min-guess mid-guess point p0 p1 p2 p3 :steps steps)
-              (refine-guess mid-guess max-guess point p0 p1 p2 p3 :steps steps))))))
+              (refine-guess left-guess mid-guess point p0 p1 p2 p3)
+              (refine-guess mid-guess right-guess point p0 p1 p2 p3))))))
 
 (defun midpoint (point-1 point-2)
   (multiple-value-bind (x1 y1)
